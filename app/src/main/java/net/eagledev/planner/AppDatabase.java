eef98f4615783f7aed969a7257e9c866f19edece
Glembinek <glembinek97@gmail.com>
2019-06-13 23:55:21 +0200

    tasks 9

--- app/src/main/java/net/eagledev/planner/AppDatabase.java
+++ app/src/main/java/net/eagledev/planner/AppDatabase.java
@@ -1,9 +1,19 @@
 package net.eagledev.planner;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.migration.Migration;
 
-@Database(entities = {Action.class, Reminder.class, Routine.class, Aim.class}, version = 4)
+@Database(entities = {Action.class, Reminder.class, Routine.class, Aim.class, Task.class}, version = 5)
 public abstract class AppDatabase extends RoomDatabase{
     public abstract AppDao appDao();
+
+    static final Migration MIGRATION_4_5 = new Migration(4, 5) {
+        @Override
+        public void migrate(SupportSQLiteDatabase database) {
+
+        }
+    };
+
 }
